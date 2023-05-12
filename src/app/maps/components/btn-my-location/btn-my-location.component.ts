651f9533cddb6abc3332dcb0d4cb0d13962a6756
Diego Salazar <dsalazar@ec.krugercorp.com>
2023-05-12 18:02:15 -0500

    Logo Angular y boton mi ubicacion

---
+++ src/app/maps/components/btn-my-location/btn-my-location.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-my-location',
+  templateUrl: './btn-my-location.component.html',
+  styleUrls: ['./btn-my-location.component.css']
+})
+export class BtnMyLocationComponent {
+
+  constructor() { }
+
+  goToMyLocation(){
+    console.log('ir a mi ubicacion');
+  }
+
+
+}
