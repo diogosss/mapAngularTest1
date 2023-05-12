d91ef7a89a36b22f8a11c702ec700068ed86cd0a
Diego Salazar <dsalazar@ec.krugercorp.com>
2023-05-11 21:36:55 -0500

    [Startup]

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+if( !navigator.geolocation){
+  alert('Navegador no soporta GEolocation');
+  throw new Error('Navegador no soporta GEolocation');
+}
+
 if (environment.production) {
   enableProdMode();
 }
