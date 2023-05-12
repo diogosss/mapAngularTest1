650d6f65a8047e21fd60de8bcb8efa80c6937456
Diego Salazar <dsalazar@ec.krugercorp.com>
2023-05-12 18:17:26 -0500

    Servicio para controlar el mapa

--- src/app/maps/components/btn-my-location/btn-my-location.component.ts
+++ src/app/maps/components/btn-my-location/btn-my-location.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MapService, PlacesService } from '../../services';
 
 @Component({
   selector: 'app-btn-my-location',
@@ -7,10 +8,20 @@ import { Component } from '@angular/core';
 })
 export class BtnMyLocationComponent {
 
-  constructor() { }
+  constructor(
+    private mapService: MapService,
+    private placesService: PlacesService
+  ) { }
 
   goToMyLocation(){
-    console.log('ir a mi ubicacion');
+    //console.log('ir a mi ubicacion');
+
+    if(!this.placesService.isUserLocationready) throw Error('No hay ubicacion de usuario');
+    if(!this.mapService.isMapReady) throw Error('No se ha inicializado el mapa');
+
+    this.mapService.flyTo( this.placesService.useLocation!);
+
+
   }
 
 
--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { PlacesService } from '../../services';
+import { MapService, PlacesService } from '../../services';
 import { Map, Popup, Marker } from 'mapbox-gl';
 
 @Component({
@@ -12,7 +12,10 @@ export class MapViewComponent implements AfterViewInit {
   @ViewChild('mapDiv')
   mapDivElement!: ElementRef
 
-  constructor( private placesService: PlacesService) { }
+  constructor(
+    private placesService: PlacesService,
+    private mapService: MapService
+    ) { }
 
   ngAfterViewInit(): void {
 
@@ -35,6 +38,9 @@ export class MapViewComponent implements AfterViewInit {
       new Marker({color:'red'})
       .setLngLat(this.placesService.useLocation)
       .addTo(map);
+
+      //
+      this.mapService.setMap(map);
 
 
   }
