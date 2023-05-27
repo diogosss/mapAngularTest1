3abab87e62f7e76532b536df8f03a8feb0b2e75b
Diego Salazar <dsalazar@ec.krugercorp.com>
2023-05-26 19:45:32 -0500

    [ZoomRange] parte1

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
+import { Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrls: ['./zoom-range-page.component.css']
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit {
 
+  @ViewChild('map') divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+
+    if(!this.divMap) throw 'El elemento html no es encontrado';
+
+    console.log(this.divMap);
+    const map = new Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-74.5,40],
+      zoom: 9,
+    });
+  }
 
 
 }
