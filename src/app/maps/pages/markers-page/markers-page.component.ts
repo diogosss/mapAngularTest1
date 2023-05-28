acbbbde08a04e96ebcd657fd3fed7b903d54a23a
Diego Salazar <dsalazar@ec.krugercorp.com>
2023-05-28 16:44:35 -0500

    Marcadores en el mapa

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
   templateUrl: './markers-page.component.html',
@@ -6,6 +7,34 @@ import { Component } from '@angular/core';
 })
 export class MarkersPageComponent{
 
+  @ViewChild('map') divMap?: ElementRef;
 
+  public zoom: number=16.5;
+  public map?: Map;
+  public currentLngLat: LngLat= new LngLat( -78.55483990951429,-0.3318670302490042);
+
+  ngAfterViewInit(): void {
+
+    if(!this.divMap) throw 'El elemento html no es encontrado';
+
+    console.log(this.divMap);
+    this.map = new Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.currentLngLat,
+      zoom: this.zoom,
+    });
+
+    // //crear markador personalizado
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Diogos';
+
+    // const marker = new Marker({
+    //   //color: 'red'
+    //   element: markerHtml
+    // })
+    // .setLngLat( this.currentLngLat).addTo(this.map);
+
+  }
 
 }
