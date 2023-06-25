0ac69b0d32ed375bea6645a150b30a934969280c
Diego Salazar <dsalazar@ec.krugercorp.com>
2023-06-25 17:04:46 -0500

    [Firebase] Conexion librerias Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  apiKey: 'pk.eyJ1IjoiZGlvZ29zc3MiLCJhIjoiY2xoanlwMXI3MDNhNTNucDh6NzhwYzU3MiJ9.71IQHqH8JePDskCYiVHVVg'
+  apiKey: 'pk.eyJ1IjoiZGlvZ29zc3MiLCJhIjoiY2xoanlwMXI3MDNhNTNucDh6NzhwYzU3MiJ9.71IQHqH8JePDskCYiVHVVg',
+  firebase: {
+    apiKey: "AIzaSyDqE29HAsVwMQQielkQs-X67ueDnyz7g9Y",
+    authDomain: "gdi-places-api.firebaseapp.com",
+    databaseURL: "https://gdi-places-api-default-rtdb.firebaseio.com",
+    projectId: "gdi-places-api",
+    storageBucket: "gdi-places-api.appspot.com",
+    messagingSenderId: "544972047954",
+    appId: "1:544972047954:web:d3f5e680c9c9247e98018b"
+  }
 };
